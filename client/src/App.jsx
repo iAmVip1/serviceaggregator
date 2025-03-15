54587fdad089e33a3cfc627f83791af6a57402a9
iAmVip1 <gp16373@gmail.com>
2025-03-15 19:47:40 +0545

    routes for pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Dashboard from './pages/Dashboard'
+import Signin from './pages/Signin'
+import Signup from './pages/Signup'
+import MyBooking from './pages/MyBooking'
+import Select from './pages/Select'
 
 export default function App() {
   return (
-    <h1 >
-      <div className="text-3xl text-amber-400">
-        hello
-      </div>
-    </h1>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/contact' element={<Contact />} />
+    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/mybooking' element={<MyBooking />} />
+    <Route path='/select' element={<Select />} />
+    <Route path='/signin' element={<Signin />} />
+    <Route path='/signup' element={<Signup />} />
+    
+   </Routes>
+   </BrowserRouter>
   )
 }
